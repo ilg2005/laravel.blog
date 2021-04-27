747d442569fecf84c02bb14796d824820e079393
ilg2005 <igoldfarb10@gmail.com>
2021-04-27 10:38:31 +0300

    posts - 1

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Category;
+use App\Http\Controllers\Controller;
+use App\Post;
+use App\Tag;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+     */
+    public function index()
+    {
+        $posts = Post::paginate(20);
+
+        return view('admin.posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+     */
+    public function create()
+    {
+        $categories = Category::pluck('title', 'id')->all();
+        $tags = Tag::pluck('title', 'id')->all();
+        return view('admin.posts.create', compact( 'categories', 'tags'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Routing\Redirector
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+        ]);
+        Post::create($request->all());
+        return redirect('admin.posts.index')->with('success', 'Статья добавлена');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        return view('admin.posts.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Routing\Redirector
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'title' => 'required',
+        ]);
+
+        return redirect('posts.index')->with('success', 'Изменения сохранены');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Routing\Redirector
+     */
+    public function destroy($id)
+    {
+        return redirect('posts.index')->with('success', 'Статья удалена');
+    }
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,15 +12,19 @@ const mix = require('laravel-mix');
  */
 
 mix.styles([
-        'resources/assets/admin/plugins/fontawesome-free/css/all.min.css',
-        'resources/assets/admin/css/adminlte.min.css',
-    ], 'public/assets/admin/css/admin.css');
+    'resources/assets/admin/css/select2-bootstrap4.min.css',
+    'resources/assets/admin/plugins/fontawesome-free/css/all.min.css',
+    'resources/assets/admin/css/select2.css',
+    'resources/assets/admin/css/adminlte.min.css',
+], 'public/assets/admin/css/admin.css');
 
 mix.scripts([
     'resources/assets/admin/plugins/jquery/jquery.min.js',
     'resources/assets/admin/plugins/bootstrap/js/bootstrap.bundle.min.js',
+    'resources/assets/admin/js/select2.full.js',
     'resources/assets/admin/js/adminlte.min.js',
     'resources/assets/admin/js/demo.js',
+    'resources/assets/admin/js/select2init.js',
 ], 'public/assets/admin/js/admin.js');
 
 mix.copyDirectory('resources/assets/admin/plugins/fontawesome-free/webfonts', 'public/assets/admin/webfonts');
