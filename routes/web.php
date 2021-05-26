eb7237bc71fc1fb5caa975ed33fcb55ba57acfc2
ilg2005 <igoldfarb10@gmail.com>
2021-05-26 14:57:00 +0300

    добавлен шаблон для фронта

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/', 'PostController@index')->name('home');
 
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'admin'], function () {
     Route::get('/', 'MainController@index')->name('admin.index');
@@ -33,3 +31,9 @@ Route::group(['middleware' => 'guest'], function () {
 
 
 Route::get('/logout', 'UserController@logout')->name('logout')->middleware('auth');
+
+
+Route::group(['middleware' => 'user'], function () {
+    Route::get('/post/index', 'PostController@index')->name('posts.index');
+    Route::get('/article', 'PostController@show')->name('posts.single');
+});
